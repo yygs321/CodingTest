e9f43ee460af5fffd87ed05bffdee0fc20cacd0e
somin <83453597+yygs321@users.noreply.github.com>
2023-02-15 01:26:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,39 +1,29 @@
-from itertools import permutations 
+#DFS
+N = int(input())
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))  # +, -, *, //
 
-def operate(n1,n2,o):
-    if o=='+':
-        return n1+n2
-    elif o=='-':
-        return n1-n2
-    elif o=='*':
-        return n1*n2
-    else:
-        return int(n1/n2)
-          
-n=int(input())
-operator="+-*/"
-num=list(map(int,input().split())) #숫자 
-lst=list(map(int,input().split())) #연산자
+maximum = -1e9
+minimum = 1e9
 
-op=""
 
-for idx,ls in enumerate(lst):
-    op+=operator[idx]*ls
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
 
-#set으로 중복 제거
-#연산자 개수: n-1
-pmt=set(permutations(op,n-1))
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
 
-first=num.pop(0) #하나 줄여서 연산자랑 개수 맞춤
 
-max_val,min_val=-1e9, 1e9
-for pm in pmt:
-    result=first #매연산 돌아갈때 첫숫자부터 다시 계산
-    for n, p in zip(num, pm):
-        result=operate(result, n, p)
-      
-    max_val=max(max_val,result)
-    min_val=min(min_val,result)
-
-print(max_val)
-print(min_val)
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
