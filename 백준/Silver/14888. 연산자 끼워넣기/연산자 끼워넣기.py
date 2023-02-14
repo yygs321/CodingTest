c39600ccf7293de3377a2e65c9926b3f8e7bfc81
somin <83453597+yygs321@users.noreply.github.com>
2023-02-15 00:30:17 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 620 ms, Memory: 36376 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,37 @@
+from itertools import permutations 
+
+n=int(input())
+operator="+-*/"
+num=list(map(int,input().split())) #숫자 
+lst=list(map(int,input().split())) #연산자
+
+op=""
+
+for idx,ls in enumerate(lst):
+    op+=operator[idx]*ls
+
+#set으로 중복 제거
+#연산자 개수: n-1
+pmt=set(permutations(op,n-1))
+
+first=num.pop(0) #하나 줄여서 연산자랑 개수 맞춤
+
+max_val,min_val= -1e9, 1e9
+for pm in pmt:
+    result=first #매연산 돌아갈때 첫숫자부터 다시 계산
+    for n, p in zip(num, pm):
+        if p=='+':
+            result+=n
+        elif p=='-':
+            result-=n
+        elif p=='*':
+            result*=n
+        else:
+            result=int(result/n)
+          
+      
+    max_val=max(max_val,result)
+    min_val=min(min_val,result)
+
+print(max_val)
+print(min_val)
