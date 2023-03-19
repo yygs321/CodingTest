4d4fd4cc917f0e7b7411f5d74032fa644acbc1a5
somin <83453597+yygs321@users.noreply.github.com>
2023-03-19 13:55:17 +0900

    [Gold III] Title: 파티, Time: 2532 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,35 +1,48 @@
+#n개의 마을과 학생, 각 마을에 한명씩 산다
+#x번 마을 왕복(오고가는 시간 다름))
+#m개의 단방향 도로 i번째 길을 지나는데 T(i)만큼의 시간 소비
+#오고가는 각각의 최단시간 중 가장 오래 걸린 시간 출력
 import heapq
-n, m, x = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-ans = [0 for _ in range(n)]
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((c, b))
+n,m,x=map(int,input().split())
+INF=int(1e9)
+graph=[[] for _ in range(n+1)]
 
+#간선 가중치가 다른 최단거리
+for i in range(m):
+    s,e,t=map(int,input().split())
+    #받을때부터 (시간, 위치)순으로 값 삽입
+    graph[s].append((t,e))
 
-def dijk(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    dist = [int(1e9) for _ in range(n + 1)]
-    dist[start] = 0
+# print(graph)
+# [[], [(2, 4), (3, 2), (4, 7)], [(1, 1), (3, 5)], [(1, 2), (4, 4)], [(2, 3)], [], [], []]
 
+def dijkstra(start):
+    q=[]
+    distance=[INF for _ in range(n+1)] #1~n
+    
+    heapq.heappush(q, (0,start))
+    distance[start]=0
+  
     while q:
-        cost, now = heapq.heappop(q)
+        time, now=heapq.heappop(q)
+    
+        for i in graph[now]:
+              next_t, next=i
+              if distance[next]> time+next_t:
+                  distance[next]=time+next_t
+                  heapq.heappush(q, (distance[next], next))
+      
+    return distance #배열로 반환
 
-        for d, next in graph[now]:
-            if dist[next] > cost + d:
-                dist[next] = cost + d
-                heapq.heappush(q, (cost + d, next))
+result=0
+#각 학생들의 거리 계산을 위한 for문
+for i in range(1,n+1):
+    #dijkstra(i) : i번째 마을에서 시작해서 각 마을까지의 최단거리 배열
+    #dijkstra(i)[x] : 그 중 x마을까지 가는데 걸린 시간
+    go=dijkstra(i)[x] 
+    back=dijkstra(x)[i] # x마을에서 i번째로 돌아오기까지의 최단시간
+  
+    result=max(result, go+back)
 
-    return dist
-
-
-r = -1
-for i in range(1, n + 1):
-    if i != x:
-        go = dijk(i)[x]
-        goal = dijk(x)[i]
-        r = max(r, go + goal)
-
-print(r)
+print(result)
