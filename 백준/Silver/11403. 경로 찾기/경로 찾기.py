66942ded6226b60f8f7b93a885270c9c6e9f764e
somin <83453597+yygs321@users.noreply.github.com>
2023-03-23 00:21:00 +0900

    [Silver I] Title: 경로 찾기, Time: 112 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,20 +1,31 @@
-#입력
-N = int(input())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
-    
-    
-#플로이드-워셜 알고리즘
-for k in range(N): #경로 for문이 가장 상위 단계여야 누락되지 않는다
-    for i in range(N):
-        for j in range(N): 
-            if graph[i][j] == 1 or (graph[i][k] == 1 and graph[k][j] == 1):
-                graph[i][j] = 1
+from collections import deque
 
+n=int(input())
+graph=[]
+for _ in range(n):
+  graph.append(list(map(int,input().split())))
 
-#출력
-for row in graph:
-    for col in row:
-        print(col, end = " ")
+result=[[0 for _ in range(n)] for _ in range(n)]
+
+queue=deque()
+
+def dfs(i,j):
+    result[i][j]=1
+    queue.append(j)
+  
+    while queue:
+        q= queue.popleft()
+        for idx,gp in enumerate(graph[q]):
+            if gp==1 and result[i][idx]==0:
+                result[i][idx]=1
+                queue.append(idx)
+  
+for i in range(n):
+    for j in range(n):
+        if graph[i][j]==1:
+            dfs(i,j)
+
+for i in range(n):
+    for j in range(n):
+        print(result[i][j], end=" ")
     print()
