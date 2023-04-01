86a823b54a75d216b70c7a33e190b026f21dee40
somin <83453597+yygs321@users.noreply.github.com>
2023-04-01 16:49:41 +0900

    [Silver I] Title: 신입 사원, Time: 4436 ms, Memory: 63832 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,29 +1,21 @@
-#서류, 면접 두개가 모두 나보다 잘난 사람이 있으면 안됨
-#선발가능한 최대 인원 수
-import heapq
+# 1946 신입사원
+import sys
+input = sys.stdin.readline
+T = int(input())
 
-test=int(input())
-for tc in range(test):
-    n=int(input())
-    employee=[]
-    cnt=1 #1등은 무조건 채용
-  
-    for i in range(n):
-        #서류 성적, 면접 순위
-        a,b=map(int,input().split())
-        heapq.heappush(employee, (a,b))
-  
-    
-    lst=[]
-    idx=1
-    a,b=heapq.heappop(employee)
-    for i in range(1,n):
-        a2,b2=heapq.heappop(employee) #a2가 a보다 서류점수는 낮은데
+for i in range(T) :
+    n = int(input())
+    people_lst = [0]*n
+    for j in range(n) :
+            t1 ,t2 = map(int,input().split())
+            people_lst[j] = [t1,t2]
 
-        if b2<b: #면접 점수는 b2가 더 높으면 
-            a=a2
-            b=b2
-            cnt+=1
-            continue
-  
-    print(cnt)
+    people_lst_sorted_0 = sorted(people_lst, key = lambda x : x[0])
+    hired = 1
+    man = people_lst_sorted_0[0][1]
+    for j in range(1,n) :
+        if people_lst_sorted_0[j][1] < man :
+            man = people_lst_sorted_0[j][1]
+            hired += 1
+
+    print(hired)
