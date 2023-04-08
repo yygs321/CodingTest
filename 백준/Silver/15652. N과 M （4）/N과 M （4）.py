fdd1f036e3fcdaf67229255fe60f1b7791d053c6
somin <83453597+yygs321@users.noreply.github.com>
2023-04-08 15:11:52 +0900

    [Silver III] Title: N과 M (4), Time: 148 ms, Memory: 116068 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+def comb(cnt):
+  global n
+  global m
+  if cnt==m:
+    print(*lst)
+    return
+
+  for i in range(1,n+1):
+    if lst and lst[-1]>i:continue
+    lst.append(i)
+    comb(cnt+1)
+    lst.pop()
+
+n,m=map(int,input().split())
+lst=[]
+comb(0)
