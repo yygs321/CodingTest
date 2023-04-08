6c74a89363ea0ff1d325993053acb6696c7fca99
somin <83453597+yygs321@users.noreply.github.com>
2023-04-08 15:03:51 +0900

    [Silver III] Title: N과 M (3), Time: 728 ms, Memory: 157224 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,14 +1,16 @@
-n,m= map(int,input().split())
- 
-s = []
- 
-def dfs():
-    if len(s)==m:
-        print(' '.join(map(str,s)))
-        return
-    
-    for i in range(1,n+1):
-        s.append(i)
-        dfs()
-        s.pop()
-dfs()
+def comb(cnt):
+  global n
+  global m
+  if cnt==m:
+    print(*lst)
+    return
+
+  for i in range(1,n+1):
+    lst.append(i)
+    comb(cnt+1)
+    lst.pop()
+
+
+n,m=map(int,input().split())
+lst=[]
+comb(0)
