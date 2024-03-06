bc6e1d9e6a5336815705116e5b00c4e71685da2c
somin <83453597+yygs321@users.noreply.github.com>
2024-03-06 23:59:04 +0900

    [Silver III] Title: 병든 나이트, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+result = 0
+# n이 1일 때 무조건 1
+if n == 1:
+    result = 1
+# n이 2일 때
+elif n == 2: 
+  if m >= 1 and m <= 6: #m이 1~6일 때
+    result = (m + 1) // 2 
+  elif m >= 7: #7이상일 때
+    result = 4
+# n이 3 이상일 때
+elif n >= 3: 
+  if m <= 6: #m이 1~6일 때
+    result = min(m, 4)
+  elif m >= 7: #m이 7 이상일 때
+    result = m - 2
+print(result)
