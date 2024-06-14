96a22249016b4881da327954b8a26816d2bd27ea
somin <83453597+yygs321@users.noreply.github.com>
2024-06-15 00:12:08 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.17 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -0,0 +1,62 @@
+from bisect import bisect_left
+def solution(n, t, m, timetable):
+    answer = ''
+    timetable.sort()
+    new_timetable=[]
+    for time in timetable:
+        hh,mm=map(int,time.split(':'))
+        hh*=60
+        new_timetable.append(hh+mm)
+    
+    current_time=540 #60*9 : 9시
+    con_time=0
+    arrived=0
+    while n>0:
+        n-=1
+        idx=bisect_left(new_timetable, current_time)
+        if n==0:
+            #맨 마지막 사람보다 버스 시간이 뒤일 경우
+            if idx>=len(new_timetable): 
+                #사람이 남아있으면
+                if arrived+m<=len(new_timetable):
+                    #다음 차례 사람보다 1분 빨리
+                    con_time=new_timetable[arrived+m-1]-1       
+                #사람이 남아있지 않으면 콘이 타면 됨
+                else:
+                    con_time=current_time
+                
+            
+            #마지막 시간이 무조건 전체 사람 이후(모두가 탈 수 있음)
+            else:
+                #현재 타고 있는 사람 수가 현재 시간에 탈 수 있는 사람 수보다 많을 때
+                if idx>arrived+m:
+                    #타고 있는 맨 마지막 사람보다 1분빨리
+                    con_time=new_timetable[arrived+m-1]-1
+
+                #현재 타고 있는 사람 수가 탈 수 있는 사람수와 같을 때
+                elif idx==arrived+m:
+                    #맨 마지막 사람보다 빨리
+                    con_time=new_timetable[arrived+m-1]-1
+
+                #현재 타고 있는 사람 수가 탈 수 있는 사람 수보다 적을 때
+                elif idx<arrived+m:
+                    if new_timetable[idx]==current_time: #딱 출발 시간이면 그보다 빨리
+                        con_time= new_timetable[idx]-1
+                    else: #idx 위치값이 출발시간 보다 뒷타임 사람 = 자리 비어있음
+                        con_time=current_time
+                        
+            arrived=min(idx,arrived+m)
+            break
+
+        if idx<arrived+m-1:
+            arrived=idx
+        arrived+=m
+        current_time+=t
+    
+    if con_time<=540 and arrived<m*n:
+        con_time=540
+    
+    con_h=con_time//60
+    con_m=con_time%60
+    answer=answer="{:02d}:{:02d}".format(con_h, con_m)
+    return answer
