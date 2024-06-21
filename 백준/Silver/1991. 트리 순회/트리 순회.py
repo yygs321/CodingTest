57de8e6b9719360094ef2d9bb6416740e18e8a65
somin <83453597+yygs321@users.noreply.github.com>
2024-06-21 11:03:08 +0900

    [Silver I] Title: 트리 순회, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,30 @@
+n = int(input().strip())
+tree = {}
+
+for _ in range(n):
+    root, left, right =(input()).strip().split()
+    tree[root] = [left, right]
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')  # root
+        preorder(tree[root][0])  # left
+        preorder(tree[root][1])  # right
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  # left
+        print(root, end='')  # root
+        inorder(tree[root][1])  # right
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  # left
+        postorder(tree[root][1])  # right
+        print(root, end='')  # root
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
