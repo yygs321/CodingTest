f0722a9420246fe3542b39e867f340e2251dc1be
somin <83453597+yygs321@users.noreply.github.com>
2024-08-25 20:46:06 +0900

    [Gold V] Title: 빗물, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,16 +1,34 @@
-r,c = map(int, input().split())
-lst = list(map(int, input().split()))
+h,w=map(int,input().split())
+lst=list(map(int,input().split()))
+max_value=max(lst)
+max_idx=lst.index(max_value)
 
-ans = 0
-for i in range(1, c - 1):
-    #각 위치에서 양 옆으로 가장 큰 값들 중
-    left = max(lst[:i])
-    right = max(lst[i+1:])
+tmp=0
+ans=0
+for i in range(1,max_idx+1):
+    if tmp==-1:
+        if lst[i]<lst[i-1]:
+            tmp=i-1
+        else:
+            tmp=i
+        continue
+    if lst[tmp]<=lst[i]:
+        for j in range(tmp, i):
+            ans+=lst[tmp]-lst[j]
+        tmp=-1
+    
 
-    #둘 중 작은 값의 높이까지만 빗물이 찬다
-    min_value = min(left, right)
-
-    if lst[i] < min_value:
-        ans += min_value - lst[i]
+tmp=w-1
+for i in range(w-2, max_idx-1,-1):
+    if tmp==-1:
+        if lst[i]<lst[i+1]:
+            tmp=i+1
+        else:
+            tmp=i
+        continue
+    if lst[tmp]<=lst[i]:
+        for j in range(i+1,tmp):
+            ans+=lst[tmp]-lst[j]
+        tmp=-1
 
 print(ans)
