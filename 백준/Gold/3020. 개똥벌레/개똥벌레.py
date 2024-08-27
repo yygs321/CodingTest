af2ced98c5dcd2ecc7954423f4e3c3f20d21a0d8
somin <83453597+yygs321@users.noreply.github.com>
2024-08-27 17:31:16 +0900

    [Gold V] Title: 개똥벌레, Time: 188 ms, Memory: 122640 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -1,35 +1,17 @@
 n, h = map(int, input().split())
-
-# 종유석 정보 저장
-down = [0] * (h+1)
-# 석순 정보 저장
-up = [0] * (h+1)
-
+lst = [0 for _ in range(h+2)]
 for i in range(n):
-    # 장애물 크기
     x = int(input())
-
-    if (i % 2 == 0):
-        down[x] += 1
-    else:
-        up[x] += 1
-
-# 인덱스를 역순으로 누적합을 계산
-for i in range(h-1, 0, -1):
-    # 높이가 i+1인건 높이 i에도 영향을주기 때문에 i+1 갯수를 i 에게도 넣어줌
-    down[i] += down[i+1]
-    up[i] += up[i+1]
-
-min_val = n  # 최대가 n
-cnt = 0
+    if i % 2 == 0:  # 석순
+        lst[1] += 1
+        lst[x+1] -= 1
+    else:  # 종유석
+        lst[h-x+1] += 1
+        lst[h+1] -= 1
 
 for i in range(1, h+1):
+    lst[i] += lst[i-1]
 
-    if (up[i] + down[h - i + 1]) < min_val:
-        min_val = up[i] + down[h - i + 1]
-        cnt = 1
-
-    elif (min_val == up[i] + down[h - i + 1]):
-        cnt += 1
-
-print(min_val, cnt)
+min_value = min(lst[1:-1])
+min_count = lst[1:-1].count(min_value)
+print(min_value, min_count)
