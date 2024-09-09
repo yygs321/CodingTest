cde8c912ece9a0c1e55eb510ff825b9cf02bef47
somin <83453597+yygs321@users.noreply.github.com>
2024-09-09 17:21:56 +0900

    [Silver II] Title: 랜선 자르기, Time: 112 ms, Memory: 110836 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,16 +1,24 @@
-import sys
-K, N = map(int, input().split())
-lan = [int(sys.stdin.readline()) for _ in range(K)]
-start, end = 1, max(lan) #이분탐색 처음과 끝위치
+k, n = map(int, input().split())
+lans = []
+for _ in range(k):
+    lans.append(int(input()))
 
-while start <= end: #적절한 랜선의 길이를 찾는 알고리즘
-    mid = (start + end) // 2 #중간 위치
-    lines = 0 #랜선 수
-    for i in lan:
-        lines += i // mid #분할 된 랜선 수
-        
-    if lines >= N: #랜선의 개수가 분기점
-        start = mid + 1
-    else:
-        end = mid - 1
-print(end)
+l = 1
+r = max(lans)
+
+ans = 0
+while l <= r:
+    mid = (l+r)//2
+
+    tmp = 0
+    for lan in lans:
+        tmp += lan//mid
+
+    if tmp < n:
+        r = mid-1
+        continue
+
+    ans = max(ans, mid)
+    l = mid+1
+
+print(ans)
