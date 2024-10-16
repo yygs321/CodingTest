33ccb3f2430fbd33584bcef754c654593c325303
somin <83453597+yygs321@users.noreply.github.com>
2024-10-17 01:00:50 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 642.80 ms, Memory: 154 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -0,0 +1,39 @@
+def solution(points, routes):
+    answer=0
+    move_dict={}
+    for route in routes:
+        time=0
+        sx,sy=0,0
+        for idx, point_num in enumerate(route[:-1]):
+            sx,sy=points[point_num-1]
+            ex,ey=points[route[idx+1]-1]
+            
+            while True:
+                if sx==ex and sy==ey:
+                    break 
+                if (sx,sy,time) in move_dict:
+                    move_dict[(sx,sy,time)]+=1
+                else:
+                    move_dict[(sx,sy,time)]=1
+                time+=1
+
+                if ex>sx: 
+                    sx+=1
+                elif ex<sx:
+                    sx-=1
+                else:
+                    if ey>sy:
+                        sy+=1
+                    elif ey<sy:
+                        sy-=1
+    
+        if (sx,sy,time) in move_dict:
+            move_dict[(sx,sy,time)]+=1
+        else:
+            move_dict[(sx,sy,time)]=1
+        
+    for val in list(move_dict.values()):
+        if val>=2:
+            answer+=1
+            
+    return answer
