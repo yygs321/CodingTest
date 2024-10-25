13e83dc6c255aa83ec8139cb058241b14edb7068
somin <83453597+yygs321@users.noreply.github.com>
2024-10-25 15:04:02 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,33 @@
 from collections import deque
+from collections import defaultdict
 
-com=int(input())
 n=int(input())
-graph=[[]*(com+1) for _ in range(com+1)]
-
+m=int(input())
+lst=defaultdict(list)
+queue=deque()
+visited=[False]*(n+1)
+maxCnt=0
+for _ in range(m):
+    s,e=map(int,input().split())
+    lst[s].append(e)
+    lst[e].append(s)
 
 def bfs(start):
-    global visited # global 정의할때 
-    visited=[] # 초기화랑 따로해야함
-    queue=deque([start])
-    visited.append(start)
+    global maxCnt
+    queue.append(start)
+    visited[start]=True
+  
     while queue:
-        x=queue.popleft()
-        for i in graph[x]:
-            if i not in visited:
-                visited.append(i)
-                queue.append(i)
-
-for i in range(n):
-    a,b=map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+        q= queue.popleft()
+  
+        for ls in lst[q]:
+            if visited[ls]==False:
+                visited[ls]=True
+                maxCnt+=1
+                queue.append(ls)
 
 bfs(1)
-        
-print(len(visited)-1) #첫 시작 1번컴퓨터 제외
+print(maxCnt)
+
+  
+  
