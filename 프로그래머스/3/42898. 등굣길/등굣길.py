3cec9835c3802c38b8c8a02e2e8d241668b2990c
somin <83453597+yygs321@users.noreply.github.com>
2024-11-01 16:01:44 +0900

    [level 3] Title: 등굣길, Time: 2.69 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,18 @@
+INF=1000000007
+def solution(m, n, puddles):
+    puddles_set={(py,px) for px,py in puddles}
+    answer = 0
+    
+    dp=[[0]*(m+1) for _ in range(n+1)]
+    dp[1][1]=1
+    
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if i==1 and j==1: continue
+            
+            if (i,j) in puddles_set:
+                dp[i][j]=0
+                continue 
+            dp[i][j]=(dp[i-1][j]+dp[i][j-1])%INF
+            
+    return (dp[n][m])%INF
