c76ecfa22d6776363dde0c22322e222231c72d8a
somin <83453597+yygs321@users.noreply.github.com>
2024-11-27 01:52:18 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 396.53 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,52 +1,65 @@
 from collections import deque
-def solution(land):
-    n=len(land)
-    m=len(land[0])
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
-    result={}
-    
 
-    def bfs(i,j,num):
-        queue=deque()
-        visited=[[False for _ in range(m)] for _ in range(n)]
-        queue.append((i,j))
-        visited[i][j]=True
-        cnt=1
-        
+def solution(land):
+    n = len(land)
+    m = len(land[0])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    oil = [[-1 for _ in range(m)] for _ in range(n)]
+    oil_num = [[-1 for _ in range(m)] for _ in range(n)]
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    current_num = 0
+
+    def bfs(i, j, num):
+        queue = deque()
+        queue.append((i, j))
+        visited[i][j] = True
+        tmp = 0
+        area = []
+
         while queue:
-            x,y=queue.popleft()
-            land[x][y]=num
-            
+            x, y = queue.popleft()
+            tmp += 1
+            area.append((x, y))
+
             for k in range(4):
-                nx=x+dx[k]
-                ny=y+dy[k]
-                if nx<0 or ny<0 or nx>=n or ny>=m: continue
-                if visited[nx][ny]==True: continue
-                if land[nx][ny]==0: continue
-                visited[nx][ny]=True
-                queue.append((nx,ny))
-                cnt+=1
-        result[num]=cnt
-                
-    num=1
+                nx = x + dx[k]
+                ny = y + dy[k]
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if visited[nx][ny]:
+                    continue
+                if land[nx][ny] == 0:
+                    continue
+
+                visited[nx][ny] = True
+                queue.append((nx, ny))
+
+        for x, y in area:
+            oil[x][y] = tmp
+            oil_num[x][y] = num
+
     for i in range(n):
         for j in range(m):
-            if land[i][j]==1:
-                num+=1
-                bfs(i,j,num)
-    
-    answer=0
-    for i in range(m):
-        answer_set=set()
-        tmp=0
-        for j in range(n):
-            if land[j][i]==0:
+            if land[i][j] == 0:
                 continue
-            answer_set.add(land[j][i])
-    
-        for a in answer_set:
-            tmp+=result[a]
-        answer=max(answer, tmp)
-        
-    return answer
+            if oil[i][j] != -1:
+                continue
+            bfs(i, j, current_num)
+            current_num += 1
+
+    answer = []
+    for j in range(m):
+        tmp = 0
+        num_set = set()
+        for i in range(n):
+            if oil[i][j] == -1:
+                continue
+            if oil_num[i][j] in num_set:
+                continue
+            tmp += oil[i][j]
+            num_set.add(oil_num[i][j])
+        answer.append(tmp)
+
+    return max(answer)
