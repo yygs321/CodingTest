f2083d411ce671987f0a6cde8d609872bd2151f1
somin <83453597+yygs321@users.noreply.github.com>
2025-01-21 21:54:49 +0900

    [Gold V] Title: 숫자고르기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,30 +1,25 @@
-N = int(input())			
-lst=[0]
-for _ in range(N):
-    lst.append(int(input()))
-answer = set()
+n=int(input())
+lst=[0]+[int(input()) for _ in range(n)]
+answer=[]
 
-# dfs 정의
-def dfs(num):
-    first.add(num)			
-    second.add(lst[num])
-  
-    if lst[num] in first:	# 이어졌던 값 중 lst[num]이 있을 때
-        if first == second:		# 첫번째 줄 집합과 두번째 줄 집합이 같으면 사이클
-            answer.update(first)	# 결과 업데이트
-            return True
-          
-        return False
-    return dfs(lst[num])	# 두번째줄 값 이어서 탐색
+def dfs(val):
+    global answer
+    if val in picked:
+        i=picked.index(val)
+        answer+=picked[i:]
+        return
 
-# dfs 실행
-for i in range(1, N+1):
-    if i not in answer:		# 결과에 없을 경우만
-        #집합 초기화
-        first=set()
-        second=set()
-        dfs(i)
+    picked.append(val)
+    dfs(lst[val])
+    return
 
+for idx, val in enumerate(lst):
+    if idx==0: continue
+    if idx in answer or val in answer: continue
+    picked = []
+    picked.append(idx)
+    dfs(val)
+
+answer=list(set(answer))
 print(len(answer))
-#리스트 한번에 출력하는 방법: print(*list)
-print(*sorted(list(answer)), sep="\n")
+print(*sorted(answer), sep='\n')
