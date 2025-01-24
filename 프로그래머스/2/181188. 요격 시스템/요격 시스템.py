d3050c06682ed6b5922216bf1a4f53083a207b8c
somin <83453597+yygs321@users.noreply.github.com>
2025-01-24 21:46:27 +0900

    [level 2] Title: 요격 시스템, Time: 519.40 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,15 +1,13 @@
 def solution(targets):
     answer = 0
-    targets.sort(key=lambda x: x[1])
-    
-    i = -1
-    e = 0
-    
-    while i < len(targets) - 1:
-        i += 1
+    targets.sort(key=lambda x:x[1])
+    i=-1
+    while i<len(targets)-1:
+        i+=1
+        target=targets[i]
         
-        if targets[i][0] >= e:
-            answer += 1
-            e = targets[i][1]
+        while i+1<len(targets) and target[1]>targets[i+1][0]:
+            i+=1
+        answer+=1
     
     return answer
