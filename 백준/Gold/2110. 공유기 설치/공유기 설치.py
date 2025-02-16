430346e8eca566b9f53c3a70a88ff1700126fb0e
somin <83453597+yygs321@users.noreply.github.com>
2025-02-17 01:48:31 +0900

    Update 공유기 설치.py

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,25 +1,27 @@
 n, c = map(int, input().split())
 house = sorted(list(int(input()) for _ in range(n)))
 
-# 공유기 사이 거리 (1이 최소)
+# 공유기 사이 거리
 l = 1
 r = house[-1]-house[0]
 result = 1
 
 while l <= r:
+    # 일단 설정한 공유기 사이의 최소 거리
     mid = (l+r)//2
     last = house[0]
-    cnt = 1  # 처음 하나는 무조건 설치
+    cnt = 1
 
     for i in range(1, n):
         if house[i] >= last+mid:
             cnt += 1
             last = house[i]
 
-    if cnt >= c:
+    if cnt < c: 
+        # 설치된 공유기 수가 적으면 최소거리가 너무 컸던 것
+        r = mid-1
+    else:
         result=max(result, mid)
         l = mid+1
-    else:
-        r = mid-1
 
 print(result)
