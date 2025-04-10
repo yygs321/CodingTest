29a605de8b649c344ef9a1bf37083936429120de
somin <83453597+yygs321@users.noreply.github.com>
2025-04-11 01:05:13 +0900

    [Gold IV] Title: 점수따먹기, Time: 168 ms, Memory: 111436 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.py
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.py
@@ -1,14 +1,32 @@
 n, m = map(int, input().split())
-prefix_sum = [[0 for _ in range(m+1)]]+[[0] +list(map(int, input().split())) for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] + prefix_sum[i][j] - prefix_sum[i-1][j-1]
+# 누적합 준비
+prefix_sum = [[0] * m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        prefix_sum[i][j] = graph[i][j]
+        if i > 0:
+            prefix_sum[i][j] += prefix_sum[i-1][j]
 
 ans = -float('inf')
-for x1 in range(1, n+1):
-    for y1 in range(1, m+1):
-        for x2 in range(x1, n+1):
-            for y2 in range(y1, m+1):
-                ans = max(ans, prefix_sum[x2][y2] - prefix_sum[x2][y1 - 1] - prefix_sum[x1 - 1][y2] + prefix_sum[x1 - 1][y1 - 1])
+
+# 모든 (x1, x2) 행 범위 고정
+for x1 in range(n):
+    for x2 in range(x1, n):
+        temp = [0] * m  # y 방향 압축 1D 배열 만들기
+        for j in range(m):
+            temp[j] = prefix_sum[x2][j]
+            if x1 > 0:
+                temp[j] -= prefix_sum[x1-1][j]
+
+        # temp 배열에서 최대 연속 부분합 (카데인 알고리즘)
+        current_sum = temp[0]
+        max_sum = temp[0]
+        for k in range(1, m):
+            current_sum = max(temp[k], current_sum + temp[k])
+            max_sum = max(max_sum, current_sum)
+        
+        ans = max(ans, max_sum)
+
 print(ans)
