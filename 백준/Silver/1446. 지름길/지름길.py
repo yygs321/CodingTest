fee35ccb2e0e3b86e9638b65578baee07971a491
somin <83453597+yygs321@users.noreply.github.com>
2025-04-16 17:56:58 +0900

    [Silver I] Title: 지름길, Time: 188 ms, Memory: 114008 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,44 +1,26 @@
-#D 킬로미터 길이 고속도로
-#운전야하는 거리의 최솟값 => 마지막까지의 거리
+from collections import defaultdict
 
-#지름길 개수 n, 고속도로 길이 d
-import heapq
+n,d=(map(int,input().split()))
 
-n,d=map(int,input().split())
+road=defaultdict(list)
+for _ in range(n):
+    s,e,k=map(int,input().split())
 
-INF=1e9
-distance=[INF]*(d+1)
-graph=[[] for _ in range(d+1)]
+    road[s].append((e,k))
 
-for i in range(d):
-    graph[i].append((i+1, 1))  
+dp=[i for i in range(d+1)]
+def dfs(end,total):
+    if end>d:
+        return
+    if dp[end]<total:
+        return
+    else:
+        dp[end]=total
 
-for i in range(n):
-    #시작위치, 도착위치, 지름길 길이
-    #거리 최솟값 -> 다익스트라 알고리즘
-    s,e,l=map(int,input().split())
-    #끝나는 지점이 고속도로 길이보다 이상이면 패스
-    if e>d: 
-      continue
-    graph[s].append((e,l))
+    for nxt in road[end]:
+        dfs(nxt[0],total+nxt[1])
+    dfs(end+1,total+1)
 
-def dijkstra(start):
-    q=[]
-    heapq.heappush(q,(0,start))
-    distance[start]=0
-  
-    while q:
-        dist, now= heapq.heappop(q)
-  
-        if dist>distance[now]:
-            continue
-    
-        for i in graph[now]:
-            cost=dist+i[1]
-            if cost<distance[i[0]]:
-                distance[i[0]]= cost
-                #제일 작은 값
-                heapq.heappush(q, (cost,i[0]))
+dfs(0,0)
+print(dp[d])
 
-dijkstra(0)
-print(distance[d])
