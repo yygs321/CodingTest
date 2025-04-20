c8e6046cd2db9d9a94dbc3cc79409ac3987d967a
somin <83453597+yygs321@users.noreply.github.com>
2025-04-21 01:09:29 +0900

    [level 2] Title: 무인도 여행, Time: 11.03 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,45 +1,50 @@
-import sys
-sys.setrecursionlimit(10**6)
-
+from collections import deque
 
 def solution(maps):
     answer = []
-    graph=[]
+    graph = []
     for map in maps:
         graph.append(list(map))
     
-    n=len(graph)
-    m=len(graph[0])
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
+    n = len(graph)
+    m = len(graph[0])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    def dfs(i,j):
-        tmp=int(graph[i][j])
+    def bfs(i, j):
+        tmp = int(graph[i][j])
+        q = deque()
+        q.append((i, j))
+        visited[i][j] = True
         
-        for k in range(4):
-            nx=i+dx[k]
-            ny=j+dy[k]
-
-            if nx<0 or ny<0 or nx>=n or ny>=m:
-                continue
-            if visited[nx][ny]:
-                continue
-            if graph[nx][ny]=='X':
-                continue
+        while q:
+            x, y = q.popleft()
+            for k in range(4):
+                nx = x + dx[k]
+                ny = y + dy[k]
                 
-            visited[nx][ny]=True
-            tmp+=dfs(nx,ny)
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                    continue
+                if visited[nx][ny]:
+                    continue
+                if graph[nx][ny] == 'X':
+                    continue
+                
+                visited[nx][ny] = True
+                tmp += int(graph[nx][ny])
+                q.append((nx, ny))
         
         return tmp
         
-    visited=[[False for _ in range(m)] for _ in range(n)]
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    
     for i in range(n):
         for j in range(m):
-            if graph[i][j]=='X':
+            if graph[i][j] == 'X':
                 continue
             if visited[i][j]:
                 continue
-            visited[i][j]=True
-            answer.append(dfs(i,j))
+            visited[i][j] = True
+            answer.append(bfs(i, j))
     
-    return list(sorted(answer)) if answer else [-1]
+    return sorted(answer) if answer else [-1]
