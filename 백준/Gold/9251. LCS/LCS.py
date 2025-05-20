954ee256a1dbcc704cc81eaecd8a5b4352fd53fd
somin <83453597+yygs321@users.noreply.github.com>
2025-05-20 23:49:21 +0900

    [Gold V] Title: LCS, Time: 120 ms, Memory: 118564 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,13 +1,16 @@
-a = list(input().rstrip())
-b = list(input().rstrip())
-n = len(a)
-m = len(b)
-dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if a[i-1] == b[j-1]:
-            dp[i][j] = dp[i-1][j-1]+1
+A = input().rstrip()
+B = input().rstrip()
+n = len(A)
+m = len(B)
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+#dp[i][j]= A의 i번째 문자까지와 B의 j번째 문자까지를 비교했을 때 LCS의 길이
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if A[i - 1] == B[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
 print(dp[n][m])
