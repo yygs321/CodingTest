e885c3beee9f850ab1a711e8738651517b6782d8
somin <83453597+yygs321@users.noreply.github.com>
2025-07-01 20:31:45 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,29 +1,21 @@
-#도착한 칸: 사다리- 더 큰 수
-#도착한 칸 : 뱀 - 작은 수
-#주사위 굴리는 횟수 최솟값
 from collections import deque
 
-INF=int(1e9)
-
 n, m = map(int, input().split())
-#이동하는 칸의 번호를 넣는다
-visited = [[INF for j in range(10)] for i in range(10)]
+visited = [[float('inf') for j in range(10)] for i in range(10)]
 
 ladder = {}
 snake = {}
 queue = deque()
 
-#사다리
 for _ in range(n):
     x, y = map(int, input().split())
     ladder[x] = y
 
-#뱀
 for _ in range(m):
     u, v = map(int, input().split())
     snake[u] = v
 
-result = INF
+result = float('inf')
 def dfs(value, count):
     global result
   
@@ -39,17 +31,17 @@ def dfs(value, count):
         return
   
     if value in list(ladder.keys()):
-        next = ladder[value]
-        dfs(next, count) #주사위굴리는게 아니니까 count 그대로
+        nxt = ladder[value]
+        dfs(nxt, count)
     elif value in list(snake.keys()):
-        next = snake[value]
-        dfs(next, count)
+        nxt = snake[value]
+        dfs(nxt, count)
     else:
         for k in range(1, 7):
             if value + k > 100:
                 break
-            next = value + k
-            dfs(next, count + 1)
+            nxt = value + k
+            dfs(nxt, count + 1)
 
 dfs(1,0)
 print(result)
