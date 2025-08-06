eefdb4ba14286acb3002896b6c381a524a76408e
somin <83453597+yygs321@users.noreply.github.com>
2025-08-06 21:21:37 +0900

    [Silver IV] Title: 수 찾기, Time: 224 ms, Memory: 136456 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,25 +1,17 @@
-N = int(input())
-A = list(map(int, input().split()))
+from bisect import bisect_left
+
+n = int(input())
+nums = list(map(int, input().split()))
+
 m = int(input())
-num= list(map(int, input().split()))
-A.sort()			# A 정렬
+existed = list(map(int, input().split()))
 
-# arr의 각 원소별로 이분탐색
-for n in num:
-    lt, rt = 0, N - 1		# lt는 맨 앞, rt는 맨 뒤
-    isExist = False		# 찾음 여부
-
-    # 이분탐색 시작
-    while lt <= rt:		# lt가 rt보다 커지면 반복문 탈출
-        mid = (lt + rt) // 2	# mid는 lt와 rt의 중간값
-        if n == A[mid]:	# num(목표값)이 A[mid]값과 같다면 (목표값 존재여부를 알았다면)
-            isExist = True	# isExist Ture 변경
-            print(1)		# 1 출력
-            break		# 반복문 탈출
-        elif n > A[mid]:	# A[mid]가 num보다 작으면
-            lt = mid + 1	# lt를 높임
-        else:			# A[mid]가 num보다 크다면
-            rt = mid - 1	# rt를 낮춤
-
-    if not isExist:		# 찾지 못한 경우
-        print(0)		# 0 출력
+nums.sort()
+for ex in existed:
+    idx = bisect_left(nums, ex)
+    if idx >= n:
+        print(0)
+    elif nums[idx] != ex:
+        print(0)
+    else:
+        print(1)
