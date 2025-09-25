8730c0c3494ed90b093671231be09c290f194431
somin <83453597+yygs321@users.noreply.github.com>
2025-09-25 22:25:46 +0900

    [level 3] Title: 입국심사, Time: 967.39 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,19 +1,18 @@
 def solution(n, times):
     answer = float('inf')
     
-    time=(max(times)*n)//len(times)
+    max_time=max(times)*n
     l=0
-    r=time
+    r=max_time
     while l<=r:
         mid=(l+r)//2
-        tmp=0
+        cnt=0
         for t in times:
-            tmp+=mid//t
-        if tmp>=n:
+            cnt+=mid//t
+        if cnt>=n:
             r=mid-1
             answer=min(answer,mid)
         else:
             l=mid+1
-    
     
     return answer
